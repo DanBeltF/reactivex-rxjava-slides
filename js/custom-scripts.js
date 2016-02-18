2b7829749348d9e8ae515cb0d071f71371e15bcd
Carl Vuorinen <carl.vuorinen@gmail.com>
2016-02-18 20:58:22 +0200

    Convert examples to es6 + remove game of life iframe

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,26 +1,11 @@
 // Add event listener to reveal.js slide changes
 Reveal.addEventListener('slidechanged', function(event) {
     // event.previousSlide, event.currentSlide, event.indexh, event.indexv
-
-    var iframeId = $(event.currentSlide).attr('data-remove-iframe');
-    if (iframeId) {
-        $('#' + iframeId).remove();
-    }
 });
 
 // Add event listener to reveal.js fragments
 Reveal.addEventListener('fragmentshown', function(event) {
     // event.fragment = the fragment DOM element
-
-    var iframeId = $(event.fragment).attr('data-add-iframe');
-    if (iframeId) {
-        $('<iframe />', {
-            name: iframeId,
-            id:   iframeId,
-            frameborder: 0,
-            src:  'http://cvuorinen.github.io/reactive-life/app/#?pattern=Acorn&color2=hotpink&interval=600&autostart&noGrid&embedded'
-        }).appendTo('.reveal');
-    }
 });
 
 function runCode(element) {
